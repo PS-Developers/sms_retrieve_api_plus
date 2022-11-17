cbca4001fc5b700de8706049a1772dee52d4926a
P693 <panneerselvam.p@novactech.in>
2022-11-17 15:22:04 +0530

    doc added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,6 @@
+/// The SMS Retriever API provides a fully automated user experience and should be used when possible. 
+/// It does, however, require you to place a custom hash code in the message body, and this may be difficult to do if you're not the sender of that message.
+
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -24,22 +27,22 @@ class _MyAppState extends State<MyApp> {
     getSignature();
   }
 
-// When your server receives a request to verify a phone number, first construct the verification message that you will send to the user's device. This message must:
+/// When your server receives a request to verify a phone number, first construct the verification message that you will send to the user's device. This message must:
 
-// 1. Be no longer than 140 bytes
-// 2. Contain a one-time code that the client sends back to your server to complete the verification flow (see Generating a one-time code)
-// 3. Include an 11-character hash string that identifies your app (see Computing your app's hash string)
+/// 1. Be no longer than 140 bytes
+/// 2. Contain a one-time code that the client sends back to your server to complete the verification flow (see Generating a one-time code)
+/// 3. Include an 11-character hash string that identifies your app (see Computing your app's hash string)
 
-// Otherwise, the contents of the verification message can be whatever you choose. It is helpful to create a message from which you can easily extract the one-time code later on. For example, a valid verification message might look like the following:
+/// Otherwise, the contents of the verification message can be whatever you choose. It is helpful to create a message from which you can easily extract the one-time code later on. For example, a valid verification message might look like the following:
 
-//Your ExampleApp code is: 123ABC78
-//FA+9qCX9VSu
+/// Your ExampleApp code is: 123ABC78
+/// FA+9qCX9VSu
 
-//Alternatively
+/// Alternatively
 
-//keytool -exportcert -alias PlayDeploymentCert -keystore MyProductionKeys.keystore | xxd -p | tr -d "[:space:]" | echo -n com.example.myapp `cat` | sha256sum | tr -d "[:space:]-" | xxd -r -p | base64 | cut -c1-11
+/// keytool -exportcert -alias PlayDeploymentCert -keystore MyProductionKeys.keystore | xxd -p | tr -d "[:space:]" | echo -n com.example.myapp `cat` | sha256sum | tr -d "[:space:]-" | xxd -r -p | base64 | cut -c1-11
 
-//Below generate key change in debug, release and publish play store generate key changed
+/// Below generate key change in debug, release and publish play store generate key changed
 
   Future<void> getSignature() async {
     String signature = "";
@@ -81,7 +84,7 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  //Get the Message using initSMSAPI Only one's triger using get meaage
+  ///Get the Message using initSMSAPI Only one's triger using get meaage
   Future<void> getMessage() async {
     String status = await SmsRetrieverApiPlus.initSMSAPI() ?? 'Unknown platform version';
     setState(() {
